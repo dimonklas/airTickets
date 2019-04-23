c69f3a00e27c45019f018e77ed62b21118467047
dimonklas <dm.martyn1311@gmail.com>
2019-04-23 12:40:10 +0300

    add screen.png

--- src/main/java/autotest/utils/listeners/AllureOnFailListener.java
+++ src/main/java/autotest/utils/listeners/AllureOnFailListener.java
@@ -1,6 +1,9 @@
 package autotest.utils.listeners;
 
 import autotest.utils.DriverUtils;
+import com.codeborne.selenide.Screenshots;
+import com.google.common.io.Files;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.listener.StepLifecycleListener;
 import io.qameta.allure.listener.TestLifecycleListener;
 import io.qameta.allure.model.StepResult;
@@ -9,6 +12,7 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import java.io.File;
 import java.io.IOException;
 
 @Log4j
@@ -59,6 +63,18 @@ public final class AllureOnFailListener implements ITestListener, StepLifecycleL
 
     @Override
     public void onFinish(final ITestContext context) {
+    }
+
+    @Attachment(type = "image/png")
+    public byte[] screenshot() throws IOException {
+        File screenshot = Screenshots.takeScreenShotAsFile();
+
+        if (screenshot == null) {
+            screenshot = new File(new File("src/main/resources/supportFiles/screen.png").getAbsolutePath());
+
+        }
+
+        return Files.toByteArray(screenshot);
     }
 
 }
